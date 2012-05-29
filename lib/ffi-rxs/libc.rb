bddea962d3e17f4257a34ab02e3f9e80ee3697d8
Chris Duncan <celldee@gmail.com>
2012-05-29 20:30:55 +0100

    Wrap module LibC in XS

--- lib/ffi-rxs/libc.rb
+++ lib/ffi-rxs/libc.rb
@@ -1,20 +1,22 @@
 # encoding: utf-8
 
-module LibC
-  extend FFI::Library
-  # figures out the correct libc for each platform including Windows
-  library = ffi_lib(FFI::Library::LIBC).first
+module XS
+  module LibC
+    extend FFI::Library
+    # figures out the correct libc for each platform including Windows
+    library = ffi_lib(FFI::Library::LIBC).first
 
-  # Size_t not working properly on Windows
-  find_type(:size_t) rescue typedef(:ulong, :size_t)
+    # Size_t not working properly on Windows
+    find_type(:size_t) rescue typedef(:ulong, :size_t)
 
-  # memory allocators
-  attach_function :malloc, [:size_t], :pointer
-  attach_function :free, [:pointer], :void
-  
-  # get a pointer to the free function; used for XS::Message deallocation
-  Free = library.find_symbol('free')
+    # memory allocators
+    attach_function :malloc, [:size_t], :pointer
+    attach_function :free, [:pointer], :void
+    
+    # get a pointer to the free function; used for XS::Message deallocation
+    Free = library.find_symbol('free')
 
-  # memory movers
-  attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
-end # module LibC
+    # memory movers
+    attach_function :memcpy, [:pointer, :pointer, :size_t], :pointer
+  end # module LibC
+end # module XS
