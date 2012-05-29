3859e426392ee496a1d0194d24b25683027c1aa6
Chris Duncan <celldee@gmail.com>
2012-05-29 16:40:30 +0100

    Revert "Change FFI::Library::LIBC call to FFI::Platform::LIBC"
    This reverts commit 3f33f94131a25a4ac47e7f487d48e421756d35a1.

--- lib/ffi-rxs/libc.rb
+++ lib/ffi-rxs/libc.rb
@@ -3,7 +3,7 @@
 module LibC
   extend FFI::Library
   # figures out the correct libc for each platform including Windows
-  library = ffi_lib(FFI::Platform::LIBC).first
+  library = ffi_lib(FFI::Library::LIBC).first
 
   # Size_t not working properly on Windows
   find_type(:size_t) rescue typedef(:ulong, :size_t)
